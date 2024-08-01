3a1b1108c8117c3172d3074256a6dc655d9941cc
Mustafa Said Bektaş <145277969+mustafasaidb@users.noreply.github.com>
2024-08-02 02:15:41 +0300

    Update Mission.js

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -6,7 +6,7 @@ function Mission() {
     <div>
     <center>
         <div className="missionContainer">
-            <img className='image' src="https://wise.com/web-art/assets/illustrations/globe-large@2x.webp"/>
+            <img className='image' src="https://wise.com/web-art/assets/illustrations/globe-large@2x.webp" alt="resim" />
             <h1 className='missionHeader'>MEET THE MONEY THAT HAS NO LIMITS</h1>
             <p className='missionParagraph'>We're developing the best way to move and manage the world's money. Minimum wage. Maximum convenience. Full throttle.</p>
             <button className='missionBtn'>More about our mission</button>
